0305efaa632c3d43e46c2ea188785f58c4c9fc63
Souparna Bose <souparnabose99@gmail.com>
2024-04-25 18:01:46 +0530

    Update project files

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -7,4 +7,8 @@ if __name__ == "__main__":
     data = {'text': ["I love cooking!", "Baking is fun", None, "Japanese cuisine is great!"]}
     df = pd.DataFrame(data)
     print(df)
+    df.dropna(subset=['text'], inplace=True)
+    print(df)
+
+    # Normalize text
 
