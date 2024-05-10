ab2d46a6618f746070824a72a0f9fcbc34700af3
Souparna Bose <souparnabose99@gmail.com>
2024-05-10 18:05:33 +0530

    Update project files

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -1,5 +1,5 @@
 # Cleaning and Preprocessing Text Data in Pandas for NLP Tasks
-
+import re
 import pandas as pd
 
 
@@ -13,3 +13,6 @@ if __name__ == "__main__":
     # Normalize text
     df['text'] = df['text'].str.lower()
     print(df)
+    df['text'] = df['text'].apply(lambda x: re.sub(r'[^\w\s]', '', x))
+    print(df)
+
