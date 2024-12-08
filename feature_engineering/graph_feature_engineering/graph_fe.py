5c895c341d944ec8e5f8ef4840cfe3464505dd98
Souparna Bose <souparnabose99@gmail.com>
2024-12-08 12:44:39 +0530

    Update project files

--- feature_engineering/graph_feature_engineering/graph_fe.py
+++ feature_engineering/graph_feature_engineering/graph_fe.py
@@ -4,8 +4,8 @@ import networkx as nx
 
 
 accounts = pd.DataFrame({
-    "user_id": [1, 2, 3],
-    "username": ["A", "B", "C"]
+    "user_id": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "username": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 })
 
 
