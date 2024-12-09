44b24a123d1eaab45e50318386a3fd01fa4d8fe9
Souparna Bose <souparnabose99@gmail.com>
2024-12-09 12:46:08 +0530

    Update project files

--- feature_engineering/graph_feature_engineering/graph_fe.py
+++ feature_engineering/graph_feature_engineering/graph_fe.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import networkx as nx
 
-
-
+# Create a dummy social media graph network
+# Create a dummy dataframe with 10 nodes
 accounts = pd.DataFrame({
     "user_id": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     "username": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 })
+
 
 
