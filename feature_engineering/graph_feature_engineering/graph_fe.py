b5a911751e44da0cfb91e6c348fe845916993a56
Souparna Bose <souparnabose99@gmail.com>
2024-12-12 13:04:29 +0530

    Update project files

--- feature_engineering/graph_feature_engineering/graph_fe.py
+++ feature_engineering/graph_feature_engineering/graph_fe.py
@@ -33,5 +33,5 @@ accounts["total_degree"] = accounts["user_id"].apply(lambda x: dir_graph.degree(
 accounts["in_degree"] = accounts["in_degree"].apply(lambda x: dir_graph.in_degree(x))
 
 # Compute out degree for each node
-accounts["out_degree"] = accounts["out_degree"].apply(lambda x: dir_graph.in_degree(x))
+accounts["out_degree"] = accounts["out_degree"].apply(lambda x: dir_graph.out_degree(x))
 
